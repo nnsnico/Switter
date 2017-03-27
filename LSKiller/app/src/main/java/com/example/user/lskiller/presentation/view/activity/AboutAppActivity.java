b5c649b444a190b0b36be30cf99a760489acc9d1
toyama_yuji <b1502433@planet.kanazawa-it.ac.jp>
2017-03-27 18:22:27 +0900

    AboutAppActivityのレイアウトを変更 @tym_freeを押すことで自分のアカウントページに遷移するようにした

--- LSKiller/app/src/main/java/com/example/user/lskiller/presentation/view/activity/AboutAppActivity.java
+++ LSKiller/app/src/main/java/com/example/user/lskiller/presentation/view/activity/AboutAppActivity.java
@@ -8,10 +8,20 @@
 
 package com.example.user.lskiller.presentation.view.activity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Html;
+import android.text.SpannableString;
+import android.text.Spanned;
+import android.text.method.LinkMovementMethod;
+import android.text.method.MovementMethod;
+import android.text.style.ClickableSpan;
 import android.transition.Slide;
+import android.view.View;
 import android.webkit.WebView;
+import android.widget.TextView;
 
 import com.example.user.lskiller.R;
 
@@ -28,5 +38,12 @@ public class AboutAppActivity extends AppCompatActivity {
 
         WebView webView = (WebView) findViewById(R.id.web_view);
         webView.loadUrl("file:///android_asset/licenses.html");
+
+        TextView myScreen = (TextView) findViewById(R.id.my_screen);
+        myScreen.setOnClickListener(v -> {
+            Intent intent =
+                new Intent(Intent.ACTION_VIEW, Uri.parse("https://twitter.com/tym_free"));
+            startActivity(intent);
+        });
     }
 }
