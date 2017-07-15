48156d51f73c51ef272dac61e9f087cf4af4da09
Unknown <yuji.jampan.blue@gmail.com>
2017-07-15 13:17:19 +0900

    TweetActivity一式作成
    Activity，layout，ViewModelを追加

--- AndroidEx/app/src/main/java/com/excercise/nns/androidex/view/activity/TweetActivity.java
+++ AndroidEx/app/src/main/java/com/excercise/nns/androidex/view/activity/TweetActivity.java
@@ -2,22 +2,30 @@ package com.excercise.nns.androidex.view.activity;
 
 import android.content.Context;
 import android.content.Intent;
+import android.databinding.DataBindingUtil;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.excercise.nns.androidex.R;
+import com.excercise.nns.androidex.databinding.ActivityTweetBinding;
+import com.excercise.nns.androidex.viewmodel.TweetViewModel;
 
 public class TweetActivity extends AppCompatActivity {
 
-    public static void start(Context context) {
+    public static void start(Context context, String screenName, long userId) {
         Intent intent = new Intent(context, TweetActivity.class);
+        intent.putExtra("screenName", screenName);
+        intent.putExtra("userId", userId);
         context.startActivity(intent);
     }
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_tweet);
+        String screenName = getIntent().getStringExtra("screenName");
+        ActivityTweetBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_tweet);
+        TweetViewModel viewModel = new TweetViewModel(screenName);
+        binding.setViewmodel(viewModel);
     }
 }
---
+++ AndroidEx/app/src/main/java/com/excercise/nns/androidex/viewmodel/TweetViewModel.java
@@ -0,0 +1,18 @@
+package com.excercise.nns.androidex.viewmodel;
+
+/**
+ * Created by nns on 2017/07/15.
+ */
+
+public class TweetViewModel {
+
+    public TweetViewModel(String screenName) {
+        if (screenName != null) {
+            // tweet
+
+        } else {
+            // reply
+
+        }
+    }
+}
