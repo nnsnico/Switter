a2c2a141a65948f7bc75151b6ddc3934453aebe9
Yuji Toyama <yuji.jampan.blue@gmail.com>
2017-09-25 23:52:12 +0900

    Add TimelineFragment and Layout

--- app/src/main/java/com/excercise/nns/androidex/view/fragment/TimelineFragment.java
+++ app/src/main/java/com/excercise/nns/androidex/view/fragment/TimelineFragment.java
@@ -1,31 +1,29 @@
-package com.excercise.nns.androidex.view.activity;
+package com.excercise.nns.androidex.view.fragment;
 
 import android.content.Context;
-import android.content.Intent;
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.os.Parcelable;
 import android.support.annotation.Nullable;
 import android.support.design.widget.Snackbar;
-import android.support.v4.content.ContextCompat;
-import android.support.v7.app.AppCompatActivity;
+import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.FrameLayout;
-import android.widget.ProgressBar;
-//import android.widget.Toast;
 
 import com.excercise.nns.androidex.R;
 import com.excercise.nns.androidex.contract.OnRecyclerListener;
 import com.excercise.nns.androidex.contract.TimelineContract;
-import com.excercise.nns.androidex.databinding.ActivityTimelineBinding;
+import com.excercise.nns.androidex.contract.TimelineFragmentCallback;
+import com.excercise.nns.androidex.databinding.FragmentTimelineBinding;
 import com.excercise.nns.androidex.model.entity.TwitterStatus;
-import com.excercise.nns.androidex.utils.TwitterUtils;
+import com.excercise.nns.androidex.view.activity.TweetActivity;
 import com.excercise.nns.androidex.view.adapter.RecyclerAdapter;
 import com.excercise.nns.androidex.view.component.RecyclerDivider;
 import com.excercise.nns.androidex.viewmodel.TimelineViewModel;
-import com.raizlabs.android.dbflow.config.FlowManager;
 
 import java.util.Collections;
 import java.util.List;
@@ -33,71 +31,66 @@ import java.util.List;
 import twitter4j.Twitter;
 
 /**
- * Created by nns on 2017/06/12.
+ * Created by nns on 2017/09/25.
  */
 
-public class TimelineActivity extends AppCompatActivity implements TimelineContract, OnRecyclerListener {
-
-    private ActivityTimelineBinding binding;
-    private TimelineViewModel viewModel;
+public class TimelineFragment extends Fragment implements TimelineContract, OnRecyclerListener {
     private Twitter twitter;
+    private FragmentTimelineBinding binding;
+    private TimelineViewModel viewModel;
     private RecyclerAdapter adapter;
+    private TimelineFragmentCallback callback;
 
-    public static void start(Context context) {
-        Intent intent = new Intent(context, TimelineActivity.class);
-        context.startActivity(intent);
+    public static TimelineFragment newInstance(Twitter twitter) {
+        Bundle args = new Bundle();
+        args.putSerializable("twitter", twitter);
+        TimelineFragment fragment = new TimelineFragment();
+        fragment.setArguments(args);
+        return fragment;
     }
 
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        FlowManager.init(this);
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof TimelineFragmentCallback) {
+            callback = (TimelineFragmentCallback) context;
+        }
+    }
 
-        twitter = TwitterUtils.getTwitterInstance(this);
-        binding =
-                DataBindingUtil.setContentView(this, R.layout.activity_timeline);
-        viewModel = new TimelineViewModel(twitter, this);
-        // toolbar setup
-        binding.toolbar.setTitle(R.string.app_name);
-        binding.toolbar.inflateMenu(R.menu.toolbar_item);
-        binding.toolbar.setTitleTextColor(ContextCompat.getColor(this, android.R.color.white));
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        super.onCreateView(inflater, container, savedInstanceState);
+
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_timeline, container, false);
+        View root = binding.getRoot();
+
+        twitter = (Twitter) getArguments().getSerializable("twitter");
+
+        viewModel = new TimelineViewModel(twitter, (TimelineContract) getContext());
+
         // recyclerView setup
         List<TwitterStatus> statuses = Collections.emptyList();
         adapter = new RecyclerAdapter(statuses);
         binding.recyclerView.setLayoutManager(
-                new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false));
-        binding.recyclerView.addItemDecoration(new RecyclerDivider(this));
+                new LinearLayoutManager(getContext(), LinearLayoutManager.VERTICAL, false));
+        binding.recyclerView.addItemDecoration(new RecyclerDivider(getContext()));
         binding.recyclerView.setAdapter(adapter);
-        binding.setViewmodel(viewModel);
-    }
+        binding.setViewModel(viewModel);
 
-    @Override
-    public void onStartOAuth() {
-        OAuthActivity.start(this);
-        finish();
-    }
-
-    @Override
-    public void onStartTweet() {
-        TweetActivity.start(this, null, 0);
-    }
-
-    @Override
-    public void onStartAbout() {
-        AboutAppActivity.start(this);
+        return root;
     }
 
     @Override
     public void loadingTimeline() {
         binding.swipeLayout.setRefreshing(false);
-        binding.setIsloaded(true);
+        callback.loadingTimeline();
     }
 
     @Override
     public void getTimelineFailed(String error) {
-        binding.setIsloaded(false);
         final Snackbar snackbar =
-                Snackbar.make(findViewById(android.R.id.content), error, Snackbar.LENGTH_INDEFINITE);
+                Snackbar.make(getView().findViewById(android.R.id.content), error, Snackbar.LENGTH_INDEFINITE);
         View view = snackbar.getView();
         FrameLayout.LayoutParams params = (FrameLayout.LayoutParams) view.getLayoutParams();
         params.gravity = Gravity.TOP;
@@ -110,21 +103,20 @@ public class TimelineActivity extends AppCompatActivity implements TimelineContr
     public void getTimelineSuccess(List<TwitterStatus> statuses) {
         adapter.setStatuses(statuses);
         adapter.notifyDataSetChanged();
-        binding.setIsloaded(false);
         Parcelable state = binding.recyclerView.getLayoutManager().onSaveInstanceState();
         binding.recyclerView.getLayoutManager().onRestoreInstanceState(state);
     }
 
     @Override
     public void postActionSuccess(String message) {
-        Snackbar snackbar = Snackbar.make(findViewById(android.R.id.content), message, Snackbar.LENGTH_LONG);
+        Snackbar snackbar = Snackbar.make(getView().findViewById(android.R.id.content), message, Snackbar.LENGTH_LONG);
         snackbar.show();
         viewModel.loadTimeline();
     }
 
     @Override
     public void postActionFailed(String error) {
-        Snackbar snackbar = Snackbar.make(findViewById(android.R.id.content), error, Snackbar.LENGTH_LONG);
+        Snackbar snackbar = Snackbar.make(getView().findViewById(android.R.id.content), error, Snackbar.LENGTH_LONG);
         snackbar.show();
     }
 
@@ -133,9 +125,10 @@ public class TimelineActivity extends AppCompatActivity implements TimelineContr
         switch (tag) {
             case "goPro":
                 // TODO: 2017/09/19 set ProfileFragment
+
                 break;
             case "reply":
-                TweetActivity.start(this, status.getScreenName(), status.getId());
+                TweetActivity.start(getContext(), status.getScreenName(), status.getId());
                 break;
             default:
                 break;
