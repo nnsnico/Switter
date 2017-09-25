38181bf2f69a1c378357a44cff1e19e21b3b5288
Yuji Toyama <yuji.jampan.blue@gmail.com>
2017-09-25 23:54:56 +0900

    Add Callback Listener

---
+++ app/src/main/java/com/excercise/nns/androidex/contract/HomeContract.java
@@ -0,0 +1,13 @@
+package com.excercise.nns.androidex.contract;
+
+/**
+ * Created by nns on 2017/09/25.
+ */
+
+public interface HomeContract {
+    void onStartOAuth();
+
+    void onStartTweet();
+
+    void onStartAbout();
+}
---
+++ app/src/main/java/com/excercise/nns/androidex/contract/TimelineFragmentCallback.java
@@ -0,0 +1,11 @@
+package com.excercise.nns.androidex.contract;
+
+/**
+ * Created by nns on 2017/09/25.
+ */
+
+public interface TimelineFragmentCallback {
+    void loadingTimeline();
+
+    void finishedTimeline();
+}
